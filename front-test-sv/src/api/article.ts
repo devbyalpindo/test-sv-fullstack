6134af82b5b1e45ec3f98f86282d5148b8da61ef
alpindo <alpindo888@gmail.com>
2022-05-17 17:07:30 +0700

    change base url

--- front-test-sv/src/api/article.ts
+++ front-test-sv/src/api/article.ts
@@ -1,63 +1,59 @@
 import callAPI from "../helpers/callApi";
 
-const baseUrl = "http://localhost:8080";
+const baseUrl = "http://test-sv:8080";
 
 export async function getAllArticle() {
-    const url = `${baseUrl}/article`;
+  const url = `${baseUrl}/article`;
 
-    return callAPI({
-        url,
-        method: "GET",
-    });
+  return callAPI({
+    url,
+    method: "GET",
+  });
 }
 
 export async function getAllArticleWithLimit(limit: number, offset: number) {
-    const url = `${baseUrl}/article/${limit}/${offset}`;
+  const url = `${baseUrl}/article/${limit}/${offset}`;
 
-    return callAPI({
-        url,
-        method: "GET",
-    });
+  return callAPI({
+    url,
+    method: "GET",
+  });
 }
 
 export async function addArticle(payload: string) {
-    const url = `${baseUrl}/article`;
+  const url = `${baseUrl}/article`;
 
-    return callAPI({
-        url,
-        method: "POST",
-        data: payload
-
-    });
+  return callAPI({
+    url,
+    method: "POST",
+    data: payload,
+  });
 }
 
 export async function editArticle(id: string, payload: string) {
-    const url = `${baseUrl}/article/${id}`;
+  const url = `${baseUrl}/article/${id}`;
 
-    return callAPI({
-        url,
-        method: "PUT",
-        data: payload
-
-    });
+  return callAPI({
+    url,
+    method: "PUT",
+    data: payload,
+  });
 }
 
 export async function getArticleById(id: string) {
-    const url = `${baseUrl}/article/${id}`;
+  const url = `${baseUrl}/article/${id}`;
 
-    return callAPI({
-        url,
-        method: "GET",
-
-    });
+  return callAPI({
+    url,
+    method: "GET",
+  });
 }
 
 export async function removeArticleById(id: number) {
-    const url = `${baseUrl}/article/${id}`;
+  const url = `${baseUrl}/article/${id}`;
 
-    return callAPI({
-        url,
-        method: "DELETE",
-
-    });
+  return callAPI({
+    url,
+    method: "DELETE",
+  });
 }
