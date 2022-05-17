60cf388640f6bd0891b28cfb15cdd5616faaadd3
alpindo <alpindo888@gmail.com>
2022-05-17 17:03:16 +0700

    change charset db

--- article.sql
+++ article.sql
@@ -31,7 +31,7 @@ CREATE TABLE `articles` (
   `created_date` timestamp NULL DEFAULT NULL,
   `updated_date` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
